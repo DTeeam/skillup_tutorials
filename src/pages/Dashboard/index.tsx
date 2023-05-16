b9c63522b11c37579319434ce3efee9a66599a5a
DTeeam <teamdetecnik@gmail.com>
2023-05-16 12:21:54 +0200

    Sales chart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,46 @@
 import DashboardLayout from 'components/ui/DashboardLayout'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import * as C3 from 'c3'
+import * as API from 'api/Api'
 
 const Dashboard: FC = () => {
-  return <DashboardLayout>Dashboard</DashboardLayout>
+  useEffect(() => {
+    ;(async () => {
+      const chart = C3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [['x'], ['Sales']],
+          types: {
+            Sales: 'bar',
+          },
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d',
+            },
+          },
+        },
+      })
+      const { data } = await API.fetchChart()
+      chart.load({
+        columns: [
+          ['x', ...data.map((r: { date: string; sum: string }) => r.date)],
+          ['Sales', ...data.map((r: { date: string; sum: string }) => r.sum)],
+        ],
+      })
+    })()
+  }, [])
+
+  return (
+    <DashboardLayout>
+      <h1 className="mb-4">Sales</h1>
+
+      <div id="chart"></div>
+    </DashboardLayout>
+  )
 }
 
 export default Dashboard
